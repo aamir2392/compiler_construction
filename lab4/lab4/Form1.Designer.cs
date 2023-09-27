b2709d84121777b1b7d5cf2044256d050a6691a8
aamir2392 <109647372+aamir2392@users.noreply.github.com>
2023-09-27 04:15:32 -0700

    Add files via upload

---
+++ lab4/lab4/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_box = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tfInput = new System.Windows.Forms.TextBox();
+            this.tfTokens = new System.Windows.Forms.RichTextBox();
+            this.activity = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // input_box
+            // 
+            this.input_box.Location = new System.Drawing.Point(32, 35);
+            this.input_box.Multiline = true;
+            this.input_box.Name = "input_box";
+            this.input_box.Size = new System.Drawing.Size(321, 45);
+            this.input_box.TabIndex = 0;
+            this.input_box.TextChanged += new System.EventHandler(this.input_box_TextChanged);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(32, 102);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(321, 181);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.btn.Location = new System.Drawing.Point(32, 310);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(321, 51);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "Lab Task";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(28, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input 1 :";
+            // 
+            // tfInput
+            // 
+            this.tfInput.Location = new System.Drawing.Point(406, 35);
+            this.tfInput.Multiline = true;
+            this.tfInput.Name = "tfInput";
+            this.tfInput.Size = new System.Drawing.Size(325, 45);
+            this.tfInput.TabIndex = 4;
+            // 
+            // tfTokens
+            // 
+            this.tfTokens.Location = new System.Drawing.Point(406, 102);
+            this.tfTokens.Name = "tfTokens";
+            this.tfTokens.Size = new System.Drawing.Size(325, 181);
+            this.tfTokens.TabIndex = 5;
+            this.tfTokens.Text = "";
+            // 
+            // activity
+            // 
+            this.activity.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.activity.Location = new System.Drawing.Point(407, 310);
+            this.activity.Name = "activity";
+            this.activity.Size = new System.Drawing.Size(324, 51);
+            this.activity.TabIndex = 6;
+            this.activity.Text = "Lab Activity";
+            this.activity.UseVisualStyleBackColor = true;
+            this.activity.Click += new System.EventHandler(this.activity_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(403, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Input 2 : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 375);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.activity);
+            this.Controls.Add(this.tfTokens);
+            this.Controls.Add(this.tfInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.input_box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input_box;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tfInput;
+        private System.Windows.Forms.RichTextBox tfTokens;
+        private System.Windows.Forms.Button activity;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lab4/lab4/Form1.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void input_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeDataGridView();
+
+        }
+
+        private void InitializeDataGridView()
+        {
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.AllowUserToDeleteRows = false;
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView.Columns.Add("Token", "Token");
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string input = input_box.Text;
+            List<string> tokens = AnalyzeInput(input);
+            DisplayTokens(tokens);
+        }
+
+        private List<string> AnalyzeInput(string input)
+        {
+            List<string> tokens = new List<string>();
+            string buffer1 = "";
+            string buffer2 = "";
+            bool usingBuffer1 = true;
+
+            foreach (char c in input)
+            {
+                if (char.IsWhiteSpace(c))
+                {
+                    usingBuffer1 = !usingBuffer1;
+                    continue;
+                }
+
+                if (usingBuffer1)
+                {
+                    buffer1 += c;
+                    buffer2 = ProcessBuffer(buffer1, tokens, buffer2);
+                    buffer1 = "";
+                }
+                else
+                {
+                    buffer2 += c;
+                    buffer1 = ProcessBuffer(buffer2, tokens, buffer1);
+                    buffer2 = "";
+                }
+            }
+
+            if (usingBuffer1)
+            {
+                ProcessBuffer(buffer1, tokens, buffer2);
+            }
+            else
+            {
+                ProcessBuffer(buffer2, tokens, buffer1);
+            }
+
+            return tokens;
+        }
+        private void DisplayTokens(List<string> tokens)
+        {
+            dataGridView.Rows.Clear();
+
+            foreach (string token in tokens)
+            {
+                dataGridView.Rows.Add(token);
+            }
+        }
+        private string ProcessBuffer(string buffer, List<string> tokens, string remainder)
+        {
+            string pattern = @"\b[a-zA-Z_]\w*\b|\b\d+(\.\d+)?\b";
+
+            MatchCollection matches = Regex.Matches(buffer + remainder, pattern);
+
+            foreach (Match match in matches)
+            {
+                tokens.Add(match.Value);
+            }
+
+            return "";
+        }
+
+        private void activity_Click(object sender, EventArgs e)
+        {
+            // Taking user input from rich textbox
+            String userInput = tfInput.Text;
+
+            // List of keywords which will be used to separate keywords from variables
+            List<String> keywordList = new List<String>();
+            keywordList.Add("int");
+            keywordList.Add("float");
+            keywordList.Add("while");
+            keywordList.Add("main");
+            keywordList.Add("if");
+            keywordList.Add("else");
+            keywordList.Add("new");
+
+            // Row is an index counter for the symbol table
+            int row = 1;
+
+            // Count is a variable to increment variable id in tokens
+            int count = 1;
+
+            // Line_num is a counter for lines in user input
+            int line_num = 0;
+
+
+            String[,] SymbolTable = new String[20, 6];
+            List<String> varListinSymbolTable = new List<String>();
+
+            // Input Buffering
+            ArrayList finalArray = new ArrayList();
+            ArrayList finalArrayc = new ArrayList();
+            ArrayList tempArray = new ArrayList();
+            char[] charinput = userInput.ToCharArray();
+
+            // Regular Expression for Variables
+            Regex variable_Reg = new Regex(@"^[A-Za-z|_][A-Za-z|0-9]*$");
+
+            // Regular Expression for Constants
+            Regex constants_Reg = new Regex(@"^[0-9]+([.][0-9]+)?([e]([+|-])?[0-9]+)?$");
+
+            // Regular Expression for Operators
+            Regex operators_Reg = new Regex(@"^[-*+/><&&||=]$");
+
+            // Regular Expression for Special_Characters
+            Regex Special_Reg = new Regex(@"^[.,'\[\]{}();:?]$");
+
+            for (int itr = 0; itr < charinput.Length; itr++)
+            {
+                Match Match_Variable = variable_Reg.Match(charinput[itr] + "");
+                Match Match_Constant = constants_Reg.Match(charinput[itr] + "");
+                Match Match_Operator = operators_Reg.Match(charinput[itr] + "");
+                Match Match_Special = Special_Reg.Match(charinput[itr] + "");
+
+                if (Match_Variable.Success || Match_Constant.Success || Match_Operator.Success || Match_Special.Success || charinput[itr].Equals(' '))
+                {
+                    tempArray.Add(charinput[itr]);
+                }
+
+                if (charinput[itr].Equals('\n'))
+                {
+                    if (tempArray.Count != 0)
+                    {
+                        int j = 0;
+                        String fin = "";
+                        for (; j < tempArray.Count; j++)
+                        {
+                            fin += tempArray[j];
+                        }
+                        finalArray.Add(fin);
+                        tempArray.Clear();
+                    }
+                }
+            }
+
+            if (tempArray.Count != 0)
+            {
+                int j = 0;
+                String fin = "";
+                for (; j < tempArray.Count; j++)
+                {
+                    fin += tempArray[j];
+                }
+                finalArray.Add(fin);
+                tempArray.Clear();
+            }
+
+            // Final Array SO far correct
+            tfTokens.Clear();
+            for (int i = 0; i < SymbolTable.GetLength(0); i++)
+            {
+                for (int j = 0; j < SymbolTable.GetLength(1); j++)
+                {
+                    SymbolTable[i, j] = ""; // Set each element to an empty string
+                }
+            }
+
+            // Looping on all lines in user input
+            for (int i = 0; i < finalArray.Count; i++)
+            {
+                String line = finalArray[i].ToString();
+                char[] lineChar = line.ToCharArray();
+                line_num++;
+
+                // Taking the current line and splitting it into lexemes by space
+                for (int itr = 0; itr < lineChar.Length; itr++)
+                {
+                    Match Match_Variable = variable_Reg.Match(lineChar[itr] + "");
+                    Match Match_Constant = constants_Reg.Match(lineChar[itr] + "");
+                    Match Match_Operator = operators_Reg.Match(lineChar[itr] + "");
+                    Match Match_Special = Special_Reg.Match(lineChar[itr] + "");
+
+                    if (Match_Variable.Success || Match_Constant.Success)
+                    {
+                        tempArray.Add(lineChar[itr]);
+                    }
+
+                    if (lineChar[itr].Equals(' '))
+                    {
+                        if (tempArray.Count != 0)
+                        {
+                            int j = 0;
+                            String fin = "";
+                            for (; j < tempArray.Count; j++)
+                            {
+                                fin += tempArray[j];
+                            }
+                            finalArrayc.Add(fin);
+                            tempArray.Clear();
+                        }
+                    }
+
+                    if (Match_Operator.Success || Match_Special.Success)
+                    {
+                        if (tempArray.Count != 0)
+                        {
+                            int j = 0;
+                            String fin = "";
+                            for (; j < tempArray.Count; j++)
+                            {
+                                fin += tempArray[j];
+                            }
+                            finalArrayc.Add(fin);
+                            tempArray.Clear();
+                        }
+                        finalArrayc.Add(lineChar[itr]);
+                    }
+                }
+
+                if (tempArray.Count != 0)
+                {
+                    String fina = "";
+                    for (int k = 0; k < tempArray.Count; k++)
+                    {
+                        fina += tempArray[k];
+                    }
+                    finalArrayc.Add(fina);
+                    tempArray.Clear();
+                }
+
+                // We have a split line here
+                for (int x = 0; x < finalArrayc.Count; x++)
+                {
+                    Match operators = operators_Reg.Match(finalArrayc[x].ToString());
+                    Match variables = variable_Reg.Match(finalArrayc[x].ToString());
+                    Match digits = constants_Reg.Match(finalArrayc[x].ToString());
+                    Match punctuations = Special_Reg.Match(finalArrayc[x].ToString());
+
+                    if (operators.Success)
+                    {
+                        // If a current lexeme is an operator then make a token e.g. <op, = >
+                        tfTokens.AppendText("<op, " + finalArrayc[x].ToString() + "> ");
+                    }
+                    else if (digits.Success)
+                    {
+                        // If a current lexeme is a digit then make a token e.g. <digit, 12.33 >
+                        tfTokens.AppendText("<digit, " + finalArrayc[x].ToString() + "> ");
+                    }
+                    else if (punctuations.Success)
+                    {
+                        // If a current lexeme is a punctuation then make a token e.g. <punc, ; >
+                        tfTokens.AppendText("<punc, " + finalArrayc[x].ToString() + "> ");
+                    }
+                    else if (variables.Success)
+                    {
+                        // If a current lexeme is a variable and not a keyword
+                        if (!keywordList.Contains(finalArrayc[x].ToString())) // If it is not a keyword
+                        {
+                            // Check what is the category of the variable, handling only two cases here
+                            // Category1- Variable initialization of type digit e.g. int count = 10 ;
+                            // Category2- Variable initialization of type String e.g. String var = ' Hello ' ;
+                            Regex reg1 = new Regex(@"^(int|String|float|double)\s([A-Za-z|_][A-Za-z|0-9]{0,10})\s(=)\s([0-9]+([.][0-9]+)?([e][+|-]?[0-9]+)?)\s(;)$"); // Line of type int alpha = 2 ;
+                            Match category1 = reg1.Match(line);
+                            Regex reg2 = new Regex(@"^(String|char)\s([A-Za-z|][A-Za-z|0-9]{0,10})\s(=)\s[']\s([A-Za-z|][A-Za-z|0-9]{0,30})\s[']\s(;)$"); // Line of type String alpha = ' Hello ' ;
+                            Match category2 = reg2.Match(line);
+
+                            // If it is a category 1 then add a row in the symbol table containing the information related to that variable
+                            if (category1.Success)
+                            {
+                                SymbolTable[row, 1] = row.ToString(); // Index
+                                SymbolTable[row, 2] = finalArrayc[x].ToString(); // Variable name
+                                SymbolTable[row, 3] = finalArrayc[x - 1].ToString(); // Type
+                                SymbolTable[row, 4] = finalArrayc[x + 2].ToString(); // Value
+                                SymbolTable[row, 5] = line_num.ToString(); // Line number
+                                tfTokens.AppendText("<var" + count + ", " + row + "> ");
+
+                                tfTokens.AppendText(SymbolTable[row, 1].ToString() + " \t ");
+                                tfTokens.AppendText(SymbolTable[row, 2].ToString() + " \t ");
+                                tfTokens.AppendText(SymbolTable[row, 3].ToString() + " \t ");
+                                tfTokens.AppendText(SymbolTable[row, 4].ToString() + " \t ");
+                                tfTokens.AppendText(SymbolTable[row, 5].ToString() + " \n ");
+                                row++;
+                                count++;
+                            }
+                            // If it is a category 2 then add a row in the symbol table containing the information related to that variable
+                            else if (category2.Success)
+                            {
+                                // If a line such as String var = ' Hello ' ; comes and the loop moves to the index of the array containing Hello,
+                                // then this if condition prevents addition of Hello in symbol Table because it is not a variable, it is just a string
+                                if (!(finalArrayc[x - 1].ToString().Equals("'") && finalArrayc[x + 1].ToString().Equals("'")))
+                                {
+                                    SymbolTable[row, 1] = row.ToString(); // Index
+                                    SymbolTable[row, 2] = finalArrayc[x].ToString(); // Varname
+                                    SymbolTable[row, 3] = finalArrayc[x - 1].ToString(); // Type
+                                    SymbolTable[row, 4] = finalArrayc[x + 3].ToString(); // Value
+                                    SymbolTable[row, 5] = line_num.ToString(); // Line number
+                                    tfTokens.AppendText("<var" + count + ", " + row + "> ");
+
+                                    tfTokens.AppendText(SymbolTable[row, 1].ToString() + " \t ");
+                                    tfTokens.AppendText(SymbolTable[row, 2].ToString() + " \t ");
+                                    tfTokens.AppendText(SymbolTable[row, 3].ToString() + " \t ");
+                                    tfTokens.AppendText(SymbolTable[row, 4].ToString() + " \t ");
+                                    tfTokens.AppendText(SymbolTable[row, 5].ToString() + " \n ");
+                                    row++;
+                                    count++;
+                                }
+                                else
+                                {
+                                    tfTokens.AppendText("<String" + count + ", " + finalArrayc[x].ToString() + "> ");
+                                }
+                            }
+                            else
+                            {
+                                // If any other category line comes in, we check if we have initialized that variable before.
+                                // If we have initialized it before, then we put the index of that variable in the symbol table in its token.
+                                String ind = "Default";
+                                String ty = "Default";
+                                String val = "Default";
+                                String lin = "Default";
+
+
+                            }
+                        }
+                        // If a current lexeme is not a variable but a keyword, then make a token such as: <keyword, int>
+                        else
+                        {
+                            tfTokens.AppendText("<keyword, " + finalArrayc[x].ToString() + "> ");
+                        }
+                    }
+                }
+                tfTokens.AppendText("\n");
+                finalArrayc.Clear();
+            }
+        }
+    }
+}
